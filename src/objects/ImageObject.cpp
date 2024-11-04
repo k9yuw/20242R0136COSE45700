28bd3f150e8dcfd43f9fa504189e2143fab3de4b
k9yuw <gyuwoneee@gmail.com>
2024-11-04 17:13:11 +0900

    [BUG] 이미지 크기 조절 되지않는 버그 수정

--- src/objects/ImageObject.cpp
+++ src/objects/ImageObject.cpp
@@ -1,9 +1,45 @@
 #include "ImageObject.h"
 
 ImageObject::ImageObject(const wxPoint& position, const wxSize& size, const wxBitmap& bitmap)
-    : CanvasObject(position, size), m_bitmap(bitmap) {
+    : CanvasObject(position, size), 
+      m_originalBitmap(bitmap), 
+      m_scaledBitmap(bitmap), 
+      m_needsRescale(false) 
+{
+    // CanvasObject의 SetOnChangeCallback을 사용하여 크기 변경 시 m_needsRescale을 true로 설정
+    SetOnChangeCallback(std::bind(&ImageObject::OnSizeChanged, this));
+}
+
+void ImageObject::OnSizeChanged() {
+    // 크기가 변경되었으므로 다음 그리기 때 스케일을 수행
+    m_needsRescale = true;
 }
 
 void ImageObject::Draw(wxDC& dc) {
-    dc.DrawBitmap(m_bitmap, m_position.x, m_position.y, true);
+    if (m_needsRescale) {
+        // 원본 비트맵을 현재 크기에 맞게 스케일링
+        wxImage image = m_originalBitmap.ConvertToImage();
+        wxImage scaledImage = image.Scale(m_size.GetWidth(), m_size.GetHeight(), wxIMAGE_QUALITY_HIGH);
+
+        // 스케일된 이미지를 다시 비트맵으로 변환
+        m_scaledBitmap = wxBitmap(scaledImage);
+
+        // 스케일이 완료되었으므로 플래그를 리셋
+        m_needsRescale = false;
+    }
+
+    // 스케일된 비트맵을 현재 위치에 그리기
+    dc.DrawBitmap(m_scaledBitmap, m_position.x, m_position.y, true);
+}
+
+bool ImageObject::isMouseNearEdge(const wxPoint& pos, int tolerance) const {
+    // 객체의 경계 근처에 마우스가 있는지 확인
+    wxRect rect(m_position, m_size);
+    return CanvasObject::isMouseNearEdge(pos, tolerance);
+}
+
+bool ImageObject::isMouseInside(const wxPoint& pos) const {
+    // 객체 내부에 마우스가 있는지 확인
+    wxRect rect(m_position, m_size);
+    return rect.Contains(pos);
 }
--- src/objects/ImageObject.h
+++ src/objects/ImageObject.h
@@ -1,12 +1,29 @@
 #pragma once
+
 #include "CanvasObject.h"
 #include <wx/bitmap.h>
+#include <wx/image.h>
+#include <functional>
 
 class ImageObject : public CanvasObject {
 public:
     ImageObject(const wxPoint& position, const wxSize& size, const wxBitmap& bitmap);
-    void Draw(wxDC& dc) override;
+    virtual ~ImageObject() {}
+
+    virtual void Draw(wxDC& dc) override;
+    virtual bool isMouseNearEdge(const wxPoint& pos, int tolerance) const override;
+    virtual bool isMouseInside(const wxPoint& pos) const override;
 
 private:
-    wxBitmap m_bitmap;
+    // 원본 비트맵을 보관
+    wxBitmap m_originalBitmap;
+
+    // 스케일된 비트맵을 캐시
+    wxBitmap m_scaledBitmap;
+
+    // 스케일이 필요한지 여부를 나타내는 플래그
+    bool m_needsRescale;
+
+    // 크기 변경 시 호출되는 콜백 함수
+    void OnSizeChanged();
 };
